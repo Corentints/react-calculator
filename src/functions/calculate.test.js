d8afa129b19a142cba81b9e55e7860b50dbaddc2
Corentin <corentin.tho@gmail.com>
2020-11-24 18:54:20 +0100

    refactor(test): refactor divisions tests  to cover all cases

--- src/functions/calculate.test.js
+++ src/functions/calculate.test.js
@@ -1,20 +1,35 @@
 import { calc } from './calculate';
 
 describe('divisions', () => {
-  test('1 / 2', () => expect(calc('1 ÷ 2')).toBe('0.5'));
-  test('2 / 1', () => expect(calc('2 ÷ 1')).toBe('2'));
+  test('two positives int', () => {
+    expect(calc('1 ÷ 2')).toBe('0.5');
+  });
 
-  test('3 / 4', () => expect(calc('3 ÷ 4')).toBe('0.75'));
-  test('4 / 3', () => expect(calc('4 ÷ 3')).toBe('1.333333'));
+  test('0 by int', () => {
+    expect(calc('0 ÷ 2')).toBe('0');
+  });
 
-  test('15 / 7', () => expect(calc('15 ÷ 7')).toBe('2.142857'));
-  test('7 / 15', () => expect(calc('7 ÷ 15')).toBe('0.4666667'));
+  test('negative int by positive int', () => {
+    expect(calc('-42 ÷ 22')).toBe('-1.909091');
+  });
 
-  test('812547 / 4582', () => expect(calc('812547 ÷ 4582')).toBe('177.3346'));
-  test('4582 / 812547', () => expect(calc('4582 ÷ 812547')).toBe('0.005639058'));
+  test('negative float by negative float', () => {
+    expect(calc('-0.42 ÷ 22.42')).toBe('-0.01873327');
+  });
 
-  test('1.23456 / 7.89101112', () => expect(calc('1.23456 ÷ 7.89101112')).toBe('0.1564514'));
-  test('7.89101112 / 1.23456', () => expect(calc('7.89101112 ÷ 1.23456')).toBe('6.39176'));
+  test('negative int by positive float', () => {
+    expect(calc('-5 ÷ 22.42')).toBe('-0.2230152');
+  });
 
-  test('5 / 0', () => expect(calc('5 ÷ 0')).toBe('Error'));
+  test('float by int', () => {
+    expect(calc('6.12 ÷ 2')).toBe('3.06');
+  });
+
+  test('float by float', () => {
+    expect(calc('6.12 ÷ 12.6')).toBe('0.4857143');
+  });
+
+  test('error if divided by 0', () => {
+    expect(calc('500 ÷ 0')).toBe('Error');
+  });
 });
